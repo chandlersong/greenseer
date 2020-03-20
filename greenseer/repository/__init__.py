acc0a79408d306ffe039ac73625e6be807c77f90
chandlersong <chandler605@gmail.com>
2020-03-20 20:06:30 +0800

    refactor code: update notebook & fix bugs

--- greenseer/repository/__init__.py
+++ greenseer/repository/__init__.py
@@ -26,6 +26,19 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+#
+#  Licensed under the GNU GENERAL PUBLIC LICENSE v3.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       https://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
 
 import abc
 import logging
@@ -165,7 +178,7 @@ class ReportRepository(RemoteFetcher):
 
     def load_data(self, stock_id, force_remote=False) -> DataFrame:
         # FUTUREIMPROVE:  add dirty check if possible.
-        if self.local_source.exist(stock_id) or force_remote:
+        if not self.local_source.exist(stock_id) or force_remote:
             self.logger.info("{} is empty, local data will be refresh".format(stock_id))
             remote_data = self.initial_remote_data(stock_id)
             self.local_source.refresh_data(remote_data, stock_id)
