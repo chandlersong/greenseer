122de4dac1521c1128d1f227042b11fcf9b8fdb1
chandlersong <chandler605@gmail.com>
2020-03-22 14:32:15 +0800

    dataset: add ut for test one task

--- greenseer/dataset/china_dataset.py
+++ greenseer/dataset/china_dataset.py
@@ -26,16 +26,37 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+#
+#  Licensed under the GNU GENERAL PUBLIC LICENSE v3.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       https://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+from functools import partial
 
 import pandas as pd
 
 from greenseer.repository.china_stock import create_china_stock_assert_repository, create_china_stock_income_repository, \
     create_china_stock_cash_repository, ChinaAssertRepository, ChinaIncomeRepository, ChinaCashRepository
 
+ASSERT_REPORT = "assert"
+
+INCOME_REPORT = "income"
+
+CASH_REPORT = "cash"
+
 DEFAULT_LOCAL_PATH = "reportData"
 
 
-class _MyRepository:
+class ChinaReportRepository:
     _fields = ["_assert", "_income", "_cash"]
 
     def __init__(self):
@@ -62,15 +83,18 @@ class _MyRepository:
         return self._cash
 
 
-_repository = _MyRepository()
+_repository = ChinaReportRepository()
 
 
 def set_local_path(local_path: str):
     _repository.refresh(local_path)
 
 
-def load_by_stock_Id(stock_id: str, force_remote=False) -> pd.DataFrame:
-    assert_report = _repository.assertReport.load_data(stock_id=stock_id, force_remote=force_remote)
-    income_report = _repository.incomeReport.load_data(stock_id=stock_id, force_remote=force_remote)
-    cash_report = _repository.cashReport.load_data(stock_id=stock_id, force_remote=force_remote)
-    return pd.concat({"assert": assert_report, "income": income_report, "cash": cash_report})
+def load_by_stock_Id(stock_id: str, force_remote=False, repository=_repository) -> pd.DataFrame:
+    assert_report = repository.assertReport.load_data(stock_id=stock_id, force_remote=force_remote)
+    income_report = repository.incomeReport.load_data(stock_id=stock_id, force_remote=force_remote)
+    cash_report = repository.cashReport.load_data(stock_id=stock_id, force_remote=force_remote)
+    return pd.concat({ASSERT_REPORT: assert_report, INCOME_REPORT: income_report, CASH_REPORT: cash_report})
+
+
+fetch_one_report = partial(load_by_stock_Id, repository=_repository)
