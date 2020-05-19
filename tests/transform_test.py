229d18462dfa465fdc6872ce2772fd558fd33ea7
chandlersong <chandler605@gmail.com>
2020-05-19 22:07:40 +0800

    transform: fix ut issue

--- tests/transform_test.py
+++ tests/transform_test.py
@@ -30,7 +30,7 @@ class RegularExpressionIndexFilterTest(unittest.TestCase):
         transformer = FunctionTransformer(func=regular_expression_index_filter, validate=False,
                                           kw_args={"pattern": r'a-\w*'})
 
-        expected = pd.DataFrame({"x": [1, 3], "y": [4, 6]}, index=["a-bb", "a-cc"])
+        expected = pd.DataFrame({"x": [3, 1], "y": [6, 4]}, index=["a-cc", "a-bb"])
 
         assert_frame_equal(expected, transformer.transform(data))
         self.assertEqual(True, True)
@@ -38,14 +38,14 @@ class RegularExpressionIndexFilterTest(unittest.TestCase):
     def test_pick_annual_report_china(self):
         data = pd.DataFrame({"x": [1, 2, 3, 4, 5], "y": [6, 7, 8, 9, 0]},
                             index=(pd.MultiIndex.from_tuples(
-                                [("a", "2020-12-30"), ("a", "2020-09-30"), ("a", "2019-12-30"), ("b", "2020-12-30"),
-                                 ("b", "2020-09-30")])))
+                                [("a", "2020-12-31"), ("a", "2020-09-31"), ("a", "2019-12-31"), ("b", "2020-12-31"),
+                                 ("b", "2020-09-31")])))
 
         transformer = FunctionTransformer(func=pick_annual_report_china, validate=False)
 
-        expected = pd.DataFrame({"x": [1, 3, 4], "y": [6, 8, 9]},
+        expected = pd.DataFrame({"x": [4, 1, 3], "y": [9, 6, 8]},
                                 index=(pd.MultiIndex.from_tuples(
-                                    [("a", "2020-12-30"), ("a", "2019-12-30"), ("b", "2020-12-30")])))
+                                    [("b", "2020-12-31"), ("a", "2020-12-31"), ("a", "2019-12-31")])))
         transform = transformer.transform(data)
         print(transform)
         assert_frame_equal(expected, transform)
@@ -59,7 +59,7 @@ class RegularExpressionColumnFilterTest(unittest.TestCase):
         transformer = FunctionTransformer(func=regular_expression_column_filter, validate=False,
                                           kw_args={"patterns": [r'我爱', r'我喜欢']})
 
-        expected = pd.DataFrame({"我爱钱": [1, 2, 3], "我喜欢古董": [4, 5, 6]}, index=["a", "b", "c"])
+        expected = pd.DataFrame({"我爱钱": [3, 2, 1], "我喜欢古董": [6, 5, 4]}, index=["c","b", "a"])
 
         assert_frame_equal(expected, transformer.transform(data))
         self.assertEqual(True, True)
